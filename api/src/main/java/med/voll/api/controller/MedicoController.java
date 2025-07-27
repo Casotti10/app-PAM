649e0bf98fcf836c8835bf68a3e923a4d9e23f2b
Lucas <lucascasotti1@gmail.com>
2025-07-27 20:21:02 -0300

    conexao com o banco de dados

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,24 +1,23 @@
-package med.voll.api.controller; // Define o pacote onde a classe está localizada
+package med.voll.api.controller;
 
-// Importações de anotações do Spring necessárias para criar endpoints REST
 import med.voll.api.medico.DadosCadastroMedico;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-//Define que esta classe é um controller REST (retorna dados diretamente, não páginas HTML)
+// Define que esta classe é um controller REST
 @RestController
-
-//Define a rota base para esse controller, ou seja, todas as URLs que começarem com "/medicos"
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
 
+    // Endpoint para cadastrar um médico (POST /medicos)
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        Medico medico = new Medico(dados); // Construtor precisa aceitar DadosCadastroMedico
+        repository.save(medico);
     }
-
-
 }
---
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,37 @@
+package med.voll.api.medico;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+
+@Table(name = "medicos") //nome na tabela no banco
+@Entity(name = "Medico") //nome entidade JPA
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //gerar id automaticamente
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING) //enum armazenado como texto no BD
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
