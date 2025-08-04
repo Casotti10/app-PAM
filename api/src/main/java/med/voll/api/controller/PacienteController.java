69afc1e33ce2903789a0a7c667f85746b402328c
Lucas <lucascasotti1@gmail.com>
2025-08-04 00:18:21 -0300

    metodo get implementado

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -3,9 +3,13 @@ package med.voll.api.controller;
 // Importações necessárias para validação, controle e transação
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,13 +20,15 @@ public class PacienteController {
     @Autowired // Injeta automaticamente uma instância de PacienteRepository
     private PacienteRepository repository;
 
-    @PostMapping // Mapeia requisições HTTP POST para este método
+    @PostMapping // Mapeia requisições HTTP POST para este metodo
     @Transactional // Garante que todas as operações com o banco sejam feitas em uma transação
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
-        // Cria um novo objeto Paciente com base nos dados recebidos do corpo da requisição
         var paciente = new Paciente(dados);
-
-        // Salva o novo paciente no banco de dados
         repository.save(paciente);
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
--- api/src/main/java/med/voll/api/paciente/Paciente.java
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -2,33 +2,35 @@ package med.voll.api.paciente;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 
 @Entity
 @Table(name = "pacientes") // nome da tabela no banco
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
 public class Paciente {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
-
     private String cpf;
 
     @Embedded
     private Endereco endereco;
 
     // Construtor recebendo o DTO
-    public Paciente(@Valid DadosCadastroPaciente dados) {
+    public Paciente (@Valid DadosCadastroPaciente dados) {
         this.nome = dados.nome();
         this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
     }
 
-    // Construtor padrão necessário para JPA
-    public Paciente() {
-    }
 
-    // Getters e Setters (se quiser usar com padrão JavaBean)
 }
